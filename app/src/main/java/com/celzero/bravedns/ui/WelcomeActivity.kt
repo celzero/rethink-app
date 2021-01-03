d1d895ed33927fdecd8c65401bfba93573613ef1
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2021-01-03 18:50:30 +0330

    Migrate WelcomeActivity.kt to viewBinding

--- app/src/main/java/com/celzero/bravedns/ui/WelcomeActivity.kt
+++ app/src/main/java/com/celzero/bravedns/ui/WelcomeActivity.kt
@@ -22,25 +22,21 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.view.*
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.celzero.bravedns.R
+import com.celzero.bravedns.databinding.ActivityWelcomeBinding
 import com.celzero.bravedns.service.PersistentState
 import org.koin.android.ext.android.inject
 
-class WelcomeActivity  : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var b: ActivityWelcomeBinding
+    private lateinit var dots: Array<TextView?>
+    internal val layout: IntArray = intArrayOf(R.layout.welcome_slide1, R.layout.welcome_slide2)
 
-    private lateinit var viewPager : ViewPager
-    private lateinit var dotsLayout: LinearLayout
-    private lateinit var dots : Array<TextView?>
-    internal val layout : IntArray = intArrayOf(R.layout.welcome_slide1,R.layout.welcome_slide2)
-
-    private lateinit var buttonNext : TextView
-    private lateinit var buttonSkip : TextView
-    private lateinit var myPagerAdapter : PagerAdapter
+    private lateinit var myPagerAdapter: PagerAdapter
 
     private val persistentState by inject<PersistentState>()
 
@@ -51,38 +47,30 @@ class WelcomeActivity  : AppCompatActivity() {
             launchHomeScreen()
         }
 
-        setContentView(R.layout.activity_welcome)
-
-
-        viewPager = findViewById(R.id.view_pager)
-        dotsLayout =  findViewById(R.id.layoutDots)
-        buttonSkip = findViewById(R.id.btn_skip)
-        buttonNext =  findViewById(R.id.btn_next)
+        b = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(b.root)
 
         addBottomDots(0)
         changeStatusBarColor()
 
         myPagerAdapter = MyPagerAdapter()
 
-        viewPager.adapter = myPagerAdapter
+        b.viewPager.adapter = myPagerAdapter
 
-        buttonSkip.setOnClickListener {
+        b.btnSkip.setOnClickListener {
             launchHomeScreen()
         }
 
-        buttonNext.setOnClickListener {
+        b.btnNext.setOnClickListener {
             val currentItem = getItem(1)
-            if(currentItem < layout.size)
-                viewPager.setCurrentItem(currentItem)
-            else
-                launchHomeScreen()
+            if (currentItem < layout.size) b.viewPager.currentItem = currentItem
+            else launchHomeScreen()
         }
 
-        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+        b.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
-                if(ViewPager.SCROLLBAR_POSITION_RIGHT == state+1){
-                    if(getItem(1) == layout.size)
-                        launchHomeScreen()
+                if (ViewPager.SCROLLBAR_POSITION_RIGHT == state + 1) {
+                    if (getItem(1) == layout.size) launchHomeScreen()
                 }
             }
 
@@ -102,31 +90,31 @@ class WelcomeActivity  : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             val window: Window = window
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.setStatusBarColor(Color.TRANSPARENT)
+            window.statusBarColor = Color.TRANSPARENT
         }
     }
 
     private fun addBottomDots(currentPage: Int) {
         dots = arrayOfNulls(layout.size)
 
-        val colorActive  = (resources.getIntArray(R.array.array_dot_active))
+        val colorActive = resources.getIntArray(R.array.array_dot_active)
         val colorInActive = resources.getIntArray(R.array.array_dot_inactive)
 
-        dotsLayout.removeAllViews()
-        for(i in dots.indices){
+        b.layoutDots.removeAllViews()
+        for (i in dots.indices) {
             dots[i] = TextView(this)
-            dots[i]?.setText(Html.fromHtml("&#8226;"))
-            dots[i]?.setTextSize(35F)
+            dots[i]?.text = Html.fromHtml("&#8226;")
+            dots[i]?.textSize = 35F
             dots[i]?.setTextColor(colorInActive[currentPage])
-            dotsLayout.addView(dots[i])
+            b.layoutDots.addView(dots[i])
         }
-        if(dots.isNotEmpty()){
+        if (dots.isNotEmpty()) {
             dots[currentPage]?.setTextColor(colorActive[currentPage])
         }
     }
 
-    private fun getItem(i : Int): Int{
-        return viewPager.currentItem+i
+    private fun getItem(i: Int): Int {
+        return b.viewPager.currentItem + i
     }
 
     private fun launchHomeScreen() {
@@ -137,7 +125,7 @@ class WelcomeActivity  : AppCompatActivity() {
 
 
     inner class MyPagerAdapter : PagerAdapter() {
-        private lateinit var layoutInflater : LayoutInflater
+        private lateinit var layoutInflater: LayoutInflater
 
         override fun isViewFromObject(view: View, `object`: Any): Boolean {
             return view == `object`
