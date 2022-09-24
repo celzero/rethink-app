e69878e38db3a258fbb472988f2e7aa0732c31f8
ignoramous <ignoramous@users.noreply.github.com>
2022-09-24 08:46:27 +0530

    AutoStart: m code comment

--- app/src/main/java/com/celzero/bravedns/receiver/BraveAutoStartReceiver.kt
+++ app/src/main/java/com/celzero/bravedns/receiver/BraveAutoStartReceiver.kt
@@ -1,18 +1,18 @@
 /*
-Copyright 2020 RethinkDNS and its authors
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2020 RethinkDNS and its authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.celzero.bravedns.receiver
 
@@ -44,6 +44,9 @@ class BraveAutoStartReceiver : BroadcastReceiver(), KoinComponent {
             return
         }
 
+        // if vpn was running before reboot, attempt an auto-start
+        // but: if always-on is enabled, then back-off, since android
+        // is expected to kick-start the vpn up on its own
         if (VpnController.state().activationRequested && !VpnController.isAlwaysOn(context)) {
             val prepareVpnIntent: Intent? = try {
                 VpnService.prepare(context)
