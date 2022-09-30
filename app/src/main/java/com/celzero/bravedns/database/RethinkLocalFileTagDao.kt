395950d9f1b279ced939e2a22e20af4042bd7189
hussainmohd-a <hussainmohd.a@gmail.com>
2022-09-30 16:31:24 +0530

    Remove rethink blocklists with no entries
    Advance blocklists will not show the blocklists with 0 entries.

--- app/src/main/java/com/celzero/bravedns/database/RethinkLocalFileTagDao.kt
+++ app/src/main/java/com/celzero/bravedns/database/RethinkLocalFileTagDao.kt
@@ -49,29 +49,29 @@ interface RethinkLocalFileTagDao {
     @Query("select * from RethinkLocalFileTag")
     fun getAllTags(): List<FileTag>
 
-    @Query("select * from RethinkLocalFileTag order by `group`")
+    @Query("select * from RethinkLocalFileTag where entries > 0 order by `group`")
     fun getLocalFileTags(): PagingSource<Int, RethinkLocalFileTag>
 
     @Query(
-        "select * from RethinkLocalFileTag where `group` in (:group) and subg in (:subg) and (vname like :query or `group` like :query or subg like :query) order by `group`")
+        "select * from RethinkLocalFileTag where entries > 0 and `group` in (:group) and subg in (:subg) and (vname like :query or `group` like :query or subg like :query) order by `group`")
     fun getLocalFileTags(query: String, group: Set<String>,
                          subg: Set<String>): PagingSource<Int, RethinkLocalFileTag>
 
     @Query(
-        "select * from RethinkLocalFileTag where `group` in (:group) and (vname like :query or `group` like :query or subg like :query) order by `group`")
+        "select * from RethinkLocalFileTag where entries > 0 and `group` in (:group) and (vname like :query or `group` like :query or subg like :query) order by `group`")
     fun getLocalFileTagsGroup(query: String,
                               group: Set<String>): PagingSource<Int, RethinkLocalFileTag>
 
     @Query(
-        "select * from RethinkLocalFileTag where subg in (:subg) and (vname like :query or `group` like :query or subg like :query) order by `group`")
+        "select * from RethinkLocalFileTag where entries > 0 and subg in (:subg) and (vname like :query or `group` like :query or subg like :query) order by `group`")
     fun getLocalFileTagsSubg(query: String,
                              subg: Set<String>): PagingSource<Int, RethinkLocalFileTag>
 
     @Query(
-        "select * from RethinkLocalFileTag where (vname like :input or `group` like :input or subg like :input) order by `group`")
+        "select * from RethinkLocalFileTag where entries > 0 and (vname like :input or `group` like :input or subg like :input) order by `group`")
     fun getLocalFileTagsWithFilter(input: String): PagingSource<Int, RethinkLocalFileTag>
 
-    @Query("select value, simpleTagId from RethinkLocalFileTag order by simpleTagId")
+    @Query("select value, simpleTagId from RethinkLocalFileTag where entries > 0 order by simpleTagId")
     fun getSimpleViewTags(): List<RethinkBlocklistManager.SimpleViewMapping>
 
     @Query("Update RethinkLocalFileTag set isSelected = 0")
