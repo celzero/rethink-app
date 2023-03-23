22c7344487066fd684b86ff463b82e869b6006f9
hussainmohd-a <hussainmohd.a@gmail.com>
2023-03-23 18:00:52 +0530

    Improvements to the Connectivity Manager to fetch UID

--- app/src/main/java/com/celzero/bravedns/net/manager/ConnectionTracer.kt
+++ app/src/main/java/com/celzero/bravedns/net/manager/ConnectionTracer.kt
@@ -6,7 +6,7 @@ import android.net.ConnectivityManager
 import android.os.Build
 import android.text.TextUtils
 import android.util.Log
-import com.celzero.bravedns.util.AndroidUidConfig.Companion.isUidAppRange
+import com.celzero.bravedns.ui.HomeScreenActivity.GlobalVariable.DEBUG
 import com.celzero.bravedns.util.Constants
 import com.celzero.bravedns.util.LoggerConstants
 import com.google.common.cache.Cache
@@ -19,6 +19,7 @@ class ConnectionTracer(ctx: Context) {
     companion object {
         private const val CACHE_BUILDER_WRITE_EXPIRE_SEC: Long = 300
         private const val CACHE_BUILDER_MAX_SIZE: Long = 1000
+        private const val PER_USER_RANGE = 100000
     }
     private val cm: ConnectivityManager
     private val uidCache: Cache<String, Int>
@@ -43,9 +44,10 @@ class ConnectionTracer(ctx: Context) {
         destIp: String,
         destPort: Int
     ): Int {
+        var uid = Constants.INVALID_UID
         // android.googlesource.com/platform/development/+/da84168fb/ndk/platforms/android-21/include/linux/in.h
         if (protocol != 6 /* TCP */ && protocol != 17 /* UDP */) {
-            return Constants.MISSING_UID
+            return uid
         }
         val local: InetSocketAddress
         val remote: InetSocketAddress
@@ -64,29 +66,28 @@ class ConnectionTracer(ctx: Context) {
                 }
         } catch (ignored: IllegalArgumentException) {
             // InetSocketAddress throws IllegalArgumentException or SecurityException
-            return Constants.MISSING_UID
+            return uid
         } catch (ignored: SecurityException) {
-            return Constants.MISSING_UID
+            return uid
         }
-        var uid = Constants.INVALID_UID
         val key = makeCacheKey(protocol, local, remote, destPort)
         try {
             uid = cm.getConnectionOwnerUid(protocol, local, remote)
-            // Cache only uid's in app range
-            if (isUidAppRange(uid)) {
+            // Returns the app id for a given uid, stripping out the user id from it.
+            // TODO: revisit this logic for multi-user support
+            // http://androidxref.com/9.0.0_r3/xref/frameworks/base/core/java/android/os/UserHandle.java#224
+            // uid %= PER_USER_RANGE
+
+            if (DEBUG) Log.d(LoggerConstants.LOG_TAG_VPN, "UID from getConnectionOwnerUid(): $uid")
+            if (uid != Constants.INVALID_UID) {
                 uidCache.put(key, uid)
-            } else {
-                // no-op
+                return uid
             }
-            return uid
         } catch (secEx: SecurityException) {
             Log.e(LoggerConstants.LOG_TAG_VPN, "NETWORK_STACK permission - " + secEx.message, secEx)
         }
         // If the uid is not in connectivity manager, then return the uid from cache.
-        try {
-            return uidCache.getIfPresent(key)!!
-        } catch (ignored: Exception) {}
-
+        uid = uidCache.getIfPresent(key) ?: Constants.INVALID_UID
         return uid
     }
 
