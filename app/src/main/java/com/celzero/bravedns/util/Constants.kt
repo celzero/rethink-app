be54505a8b58a9955260f158517ced691221b12c
hussainmohd-a <hussainmohd.a@gmail.com>
2023-08-13 20:11:45 +0530

    updated filetag.json

--- app/src/main/java/com/celzero/bravedns/util/Constants.kt
+++ app/src/main/java/com/celzero/bravedns/util/Constants.kt
@@ -127,8 +127,8 @@ class Constants {
 
         const val RETHINK_SEARCH_URL = "https://rethinkdns.com/search?s="
 
-        // default filetag.json for remote blocklist (stored in assets folder) (v054c)
-        const val PACKAGED_REMOTE_FILETAG_TIMESTAMP: Long = 1682978161602
+        // default filetag.json for remote blocklist (stored in assets folder) (v055)
+        const val PACKAGED_REMOTE_FILETAG_TIMESTAMP: Long = 1690926960536
 
         // rethinkdns sponsor link
         const val RETHINKDNS_SPONSOR_LINK = "https://svc.rethinkdns.com/r/sponsor"
