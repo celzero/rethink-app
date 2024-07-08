1ac297d0c1591326d2ae80712745c195ae67ed31
hussainmohd-a <hussainmohd.a@gmail.com>
2024-07-08 20:04:41 +0530

    logger: stop console logging after prolonged runtime

--- app/src/full/java/com/celzero/bravedns/scheduler/PurgeConsoleLogs.kt
+++ app/src/full/java/com/celzero/bravedns/scheduler/PurgeConsoleLogs.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.celzero.bravedns.database.ConsoleLogRepository
+import com.celzero.bravedns.service.PersistentState
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import java.util.concurrent.TimeUnit
@@ -12,9 +13,11 @@ class PurgeConsoleLogs(val context: Context, workerParameters: WorkerParameters)
     CoroutineWorker(context, workerParameters), KoinComponent {
 
     private val consoleLogRepository by inject<ConsoleLogRepository>()
+    private val persistentState by inject<PersistentState>()
 
     companion object {
         const val MAX_TIME: Long = 4
+        const val MAX_LOGS: Int = 200000
     }
     override suspend fun doWork(): Result {
         // delete logs which are older than MAX_TIME hrs
@@ -23,6 +26,13 @@ class PurgeConsoleLogs(val context: Context, workerParameters: WorkerParameters)
         val time = currTime - threshold
 
         consoleLogRepository.deleteOldLogs(time)
+        if (persistentState.consoleLogEnabled) {
+            // check if the logs are too many, if so stop the logging
+            val count = consoleLogRepository.getLogCount()
+            if (count > MAX_LOGS) {
+                persistentState.consoleLogEnabled = false
+            }
+        }
         return Result.success()
     }
 
