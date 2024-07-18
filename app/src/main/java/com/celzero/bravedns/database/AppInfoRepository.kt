1c936d36f7f0ce0ddd7c7da2a700d47399d53e2e
hussainmohd-a <hussainmohd.a@gmail.com>
2024-07-18 17:37:20 +0530

    repository methods to suspend functions, despite being called from IO

--- app/src/main/java/com/celzero/bravedns/database/AppInfoRepository.kt
+++ app/src/main/java/com/celzero/bravedns/database/AppInfoRepository.kt
@@ -86,23 +86,23 @@ class AppInfoRepository(private val appInfoDAO: AppInfoDAO) {
         return appInfoDAO.deleteByUid(uid)
     }
 
-    fun getDataUsageByUid(uid: Int): DataUsage {
+    suspend fun getDataUsageByUid(uid: Int): DataUsage {
         return appInfoDAO.getDataUsageByUid(uid)
     }
 
-    fun updateDataUsageByUid(uid: Int, uploadBytes: Long, downloadBytes: Long) {
+    suspend fun updateDataUsageByUid(uid: Int, uploadBytes: Long, downloadBytes: Long) {
         appInfoDAO.updateDataUsageByUid(uid, uploadBytes, downloadBytes)
     }
 
-    fun updateProxyExcluded(uid: Int, isProxyExcluded: Boolean) {
+    suspend fun updateProxyExcluded(uid: Int, isProxyExcluded: Boolean) {
         appInfoDAO.updateProxyExcluded(uid, isProxyExcluded)
     }
 
-    fun resetRethinkAppFirewallMode() {
+    suspend fun resetRethinkAppFirewallMode() {
         appInfoDAO.resetRethinkAppFirewallMode()
     }
 
-    fun getAppInfoUidForPackageName(packageName: String): Int {
+    suspend fun getAppInfoUidForPackageName(packageName: String): Int {
         return appInfoDAO.getAppInfoUidForPackageName(packageName)
     }
 }
