db9792bac4e84223d1d0490045a96f89aeda5bf7
VishnuSanal <t.v.s10123@gmail.com>
2024-09-20 18:45:43 +0530

    update colors

--- app/src/full/java/com/celzero/bravedns/ui/ui/theme/Theme.kt
+++ app/src/full/java/com/celzero/bravedns/ui/ui/theme/Theme.kt
@@ -9,18 +9,19 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF636363),
+    secondary = Color(0xFF444444),
+    tertiary = Color(0xFF18ffff)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF636363),
+    secondary = Color(0xFF444444),
+    tertiary = Color(0xFF18ffff)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
