0b8f10802f72aa192b019b6a68c1cbbaccacec70
hussainmohd-a <hussainmohd.a@gmail.com>
2025-07-01 22:21:34 +0530

    logger: improved logging in probecontroller.kt

--- app/src/main/java/com/celzero/bravedns/service/ProbeController.kt
+++ app/src/main/java/com/celzero/bravedns/service/ProbeController.kt
@@ -19,20 +19,39 @@ import Logger.LOG_TAG_CONNECTION
 import android.net.Network
 import com.celzero.firestack.intra.Controller
 
-class ProbeController(val networkHandle: Long, val nw: Network): Controller {
+class ProbeController(val networkHandle: Long, val nw: Network?): Controller {
 
     override fun bind4(who: String?, addrport: String?, fd: Long) {
+        if (networkHandle == -1L || nw == null) {
+            // no need to bind, just protect the fd
+            VpnController.protectFdForConnectivityChecks(fd)
+            Logger.i(LOG_TAG_CONNECTION, "bind4: no networkHandle or nw, fd: $fd, addr: $addrport")
+            return
+        }
         val bound = VpnController.bindToNwForConnectivityChecks(nw, fd)
-        Logger.i(LOG_TAG_CONNECTION, "bind4: bound?$bound, fd: $fd, addr: $addrport, nw: $networkHandle")
+        Logger.i(LOG_TAG_CONNECTION, "bind4: bound?$bound, fd: $fd, addr: $addrport, nw: $networkHandle, ${netId(networkHandle)})")
     }
 
     override fun bind6(who: String?, addrport: String?, fd: Long) {
+        if (networkHandle == -1L || nw == null) {
+            // no need to bind, just protect the fd
+            VpnController.protectFdForConnectivityChecks(fd)
+            Logger.i(LOG_TAG_CONNECTION, "bind6: no networkHandle or nw, fd: $fd, addr: $addrport")
+            return
+        }
         val bound = VpnController.bindToNwForConnectivityChecks(nw, fd)
-        Logger.i(LOG_TAG_CONNECTION, "bind6: bound?$bound, fd: $fd, addr: $addrport, nw: $networkHandle")
+        Logger.i(LOG_TAG_CONNECTION, "bind6: bound?$bound, fd: $fd, addr: $addrport, nw: $networkHandle, ${netId(networkHandle)})")
     }
 
     override fun protect(who: String?, fd: Long) {
         VpnController.protectFdForConnectivityChecks(fd)
-        Logger.i(LOG_TAG_CONNECTION, "protect: who: $who fd: $fd")
+        Logger.i(LOG_TAG_CONNECTION, "protect: who: $who fd: $fd, nw: $networkHandle, ${netId(networkHandle)})")
+    }
+
+    fun netId(nwHandle: Long?): Long {
+        if (nwHandle == null) {
+            return -1L
+        }
+        return nwHandle shr (32)
     }
 }
