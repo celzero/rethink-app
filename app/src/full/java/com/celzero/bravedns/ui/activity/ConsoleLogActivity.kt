6873a7659c32ee3cbfa5635ddd291ee132103815
hussainmohd-a <hussainmohd.a@gmail.com>
2025-08-01 14:47:53 +0530

    ui: optimize imports in console log activity

--- app/src/full/java/com/celzero/bravedns/ui/activity/ConsoleLogActivity.kt
+++ app/src/full/java/com/celzero/bravedns/ui/activity/ConsoleLogActivity.kt
@@ -51,7 +51,6 @@ import com.celzero.bravedns.viewmodel.ConsoleLogViewModel
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
