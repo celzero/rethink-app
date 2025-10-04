5a67fd51be592d3d9e65436b9b3ab9eada53bbb9
hussainmohd-a <hussainmohd.a@gmail.com>
2025-10-04 16:23:55 +0530

    move SsidAdapter.kt to full source set

--- app/src/full/java/com/celzero/bravedns/adapter/SsidAdapter.kt
+++ app/src/full/java/com/celzero/bravedns/adapter/SsidAdapter.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2025 RethinkDNS and its authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.celzero.bravedns.adapter
 
 import android.view.LayoutInflater
@@ -22,7 +7,6 @@ import com.celzero.bravedns.R
 import com.celzero.bravedns.data.SsidItem
 import com.celzero.bravedns.databinding.ItemSsidBinding
 import com.celzero.bravedns.util.UIUtils
-import com.celzero.bravedns.util.Utilities
 
 class SsidAdapter(
     private val ssidItems: MutableList<SsidItem>,
