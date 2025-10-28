9ac8c951cb1add5e3371da08356a09ec2135d54f
hussainmohd-a <hussainmohd.a@gmail.com>
2025-10-28 23:28:52 +0530

    add CrashReporter for Go-level crash reporting

--- app/src/fdroid/java/com/celzero/bravedns/util/FirebaseErrorReporting.kt
+++ app/src/fdroid/java/com/celzero/bravedns/util/FirebaseErrorReporting.kt
@@ -25,7 +25,7 @@ import org.koin.core.component.KoinComponent
  */
 object FirebaseErrorReporting : KoinComponent {
 
-    const val TOKEN_REGENERATION_PERIOD_DAYS = 45
+    const val TOKEN_REGENERATION_PERIOD_DAYS: Long = 45
     const val TOKEN_LENGTH = 16
     /**
      * Initialize Firebase Crashlytics - No-op for F-Droid variant
