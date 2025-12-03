190e11aeb2202c9c6bf81d7a65c096207bc39a66
hussainmohd-a <hussainmohd.a@gmail.com>
2025-12-03 21:37:27 +0530

    suppress MagicNumber warning in KnownPorts.kt

--- app/src/main/java/com/celzero/bravedns/util/KnownPorts.kt
+++ app/src/main/java/com/celzero/bravedns/util/KnownPorts.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+@file:Suppress("MagicNumber")
 package com.celzero.bravedns.util
 
 class KnownPorts {
