bc97b5247cb5510305295a4a2c209dfeae8d4f9a
hussainmohd-a <hussainmohd.a@gmail.com>
2026-01-12 20:40:19 +0530

    ui: add missing import

--- app/src/full/java/com/celzero/bravedns/ui/fragment/AboutFragment.kt
+++ app/src/full/java/com/celzero/bravedns/ui/fragment/AboutFragment.kt
@@ -25,6 +25,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.os.SystemClock
 import android.provider.Settings
