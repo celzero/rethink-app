ee8d9dde0ec45a2d9d666bfebeeba3676dcd6c08
hussainmohd-a <hussainmohd.a@gmail.com>
2026-05-16 00:12:04 +0530

    headless: rmv headless check in tile service

--- app/src/main/java/com/celzero/bravedns/service/BraveTileService.kt
+++ app/src/main/java/com/celzero/bravedns/service/BraveTileService.kt
@@ -26,7 +26,6 @@ import android.os.Build
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import androidx.annotation.RequiresApi
-import com.celzero.bravedns.ui.PrepareVpnActivity
 import com.celzero.bravedns.ui.activity.AppLockActivity
 import com.celzero.bravedns.ui.activity.MiscSettingsActivity
 import com.celzero.bravedns.util.Utilities
@@ -104,12 +103,7 @@ class BraveTileService : TileService(), KoinComponent {
             VpnController.start(this)
         } else {
             // open the app to handle the VPN start or stop
-            val intent =
-                if (Utilities.isHeadlessFlavour()) {
-                    Intent(this, PrepareVpnActivity::class.java)
-                } else {
-                    Intent(this, AppLockActivity::class.java)
-                }
+            val intent = Intent(this, AppLockActivity::class.java)
             val pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
             try {
