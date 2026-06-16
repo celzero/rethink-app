ef0ce301c752f698d11f8807723ea14824bb7fa1
hussainmohd-a <hussainmohd.a@gmail.com>
2026-06-16 12:22:48 +0530

    logs: read logs ordered by time asc

--- app/src/main/java/com/celzero/bravedns/database/ConsoleLogDAO.kt
+++ app/src/main/java/com/celzero/bravedns/database/ConsoleLogDAO.kt
@@ -29,7 +29,7 @@ interface ConsoleLogDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertBatch(log: List<ConsoleLog>)
 
-    @Query("SELECT * FROM ConsoleLog where id > :lastId ORDER BY id DESC LIMIT :limit OFFSET :offset")
+    @Query("SELECT * FROM ConsoleLog where id > :lastId LIMIT :limit OFFSET :offset")
     suspend fun getLogsChunked(lastId: Int, limit: Int, offset: Int): List<ConsoleLog>
 
     // Paged query filtered by search text AND log level (level <= :maxLevel means show
