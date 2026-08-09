4cad8f95232a103fce53e71cbf66c3a54fdf7b72
Ruben <125226745+ayeeeruben@users.noreply.github.com>
2026-08-09 01:30:08 -0700

    Update app/src/full/java/com/celzero/bravedns/adapter/FirewallAppListAdapter.kt
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/src/full/java/com/celzero/bravedns/adapter/FirewallAppListAdapter.kt
+++ app/src/full/java/com/celzero/bravedns/adapter/FirewallAppListAdapter.kt
@@ -123,7 +123,7 @@ class FirewallAppListAdapter(
                         b.firewallAppLabelTv.setTextColor(userAppColor)
                     } */
                     b.firewallAppLabelTv.text = appInfo.appName
-                    b.firewallAppInfo.text = if (appInfo.packageName.startsWith(NO_PACKAGE_PREFIX)) {
+b.firewallAppInfo.text = if (appInfo.packageName.startsWith(NO_PACKAGE_PREFIX)) {
                         context.getString(R.string.app_id_uid_only, appInfo.uid)
                     } else {
                         context.getString(R.string.app_id_package, appInfo.uid, appInfo.packageName)
